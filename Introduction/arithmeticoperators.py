7687b6f3fec32a149ec895bc9871638cdab7f3c6
Anujin Baatartsogt <35764723+anuushka@users.noreply.github.com>
2026-03-25 13:11:23 +0800

    Create arithmeticoperators.py

---
+++ Introduction/arithmeticoperators.py
@@ -0,0 +1,20 @@
+'''
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Example
+a = 3 
+b = 5
+Print the following:
+8
+-2
+15
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{(lambda a,b: a+b)(a,b)}\n{(lambda a,b: a-b)(a,b)}\n{(lambda a,b: a * b)(a,b)}")
