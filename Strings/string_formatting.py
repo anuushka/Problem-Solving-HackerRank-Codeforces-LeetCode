66ab0bc410289b29ea82b1f8795ff563b2294b4c
Anujin Baatartsogt <35764723+anuushka@users.noreply.github.com>
2026-04-06 13:48:38 +0800

    Update string_formatting.py

--- Strings/string_formatting.py
+++ Strings/string_formatting.py
@@ -47,7 +47,7 @@ def print_formatted(number):
     for i in range(number):
         d = str(i+1).rjust(width)
         o = str(oct(i+1)[2:]).rjust(width)
-        h = str(hex(i+1)[2:]).rjust(width)
+        h = str(hex(i+1)[2:].upper()).rjust(width)
         b = str(bin(i+1)[2:]).rjust(width)        
         print(d, o, h, b)        
         
